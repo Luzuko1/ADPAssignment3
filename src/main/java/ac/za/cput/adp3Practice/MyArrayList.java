824f0dd67c5d45540580beb423fa1e201e66447c
Luzuko1 <33414130+Luzuko1@users.noreply.github.com>
2019-03-08 20:52:50 +0100

    Add files via upload

---
+++ src/main/java/ac/za/cput/adp3Practice/MyArrayList.java
@@ -0,0 +1,12 @@
+package ac.za.cput.adp3Practice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MyArrayList
+{
+    public static void main(String[] args)
+    {
+        List<String> list = Arrays.asList{""}
+    }
+}
